5f0f8636cb332f59b3bbbe282a2e65566501efe0
valenmaza <vmaza.24@est.ucab.edu.ve>
2025-07-07 21:11:12 -0400

    Add files via upload

--- Controlador/ControladorLaboratorio/LaboratorioController.py
+++ Controlador/ControladorLaboratorio/LaboratorioController.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponse
+from Modelo.ModeloFarmacia.Farmacias.models import Farmacia
 from Modelo.ModeloLaboratorio.Laboratorios.forms import NuevoLaboratorio
 from Modelo.ModeloLaboratorio.Laboratorios.models import Laboratorios
+from Modelo.ModeloMedicamentos.Medicamentos.models import Medicamento
 
 # Create your views here.
 def labs(request):
@@ -18,4 +20,25 @@ def crear_labs(request):
     else:
             Laboratorios.objects.create(nombre=request.POST['nombre'], direccion=request.POST['direccion'])
             return redirect('/labs/')
+
+def modificar_labs(request, id):
+    laboratorios = get_object_or_404(Laboratorios, pk=id)
+    if request.method == 'POST':
+        form = NuevoLaboratorio(request.POST, instance=laboratorios)
+        if form.is_valid():
+            form.save()
+            return redirect('labs')
+    else:
+        form = NuevoLaboratorio(instance=laboratorios)
     
+    return render(request, 'modificar_labs.html', {'form': form, 'lab': laboratorios})
+
+def ver_historial(request, id):
+    laboratorio = get_object_or_404(Laboratorios, pk=id)
+    medicamentos = Medicamento.objects.filter(laboratorio=laboratorio)
+    farmacias = Farmacia.objects.filter(laboratorios=laboratorio)
+    return render(request, 'historial_labs.html', {
+        'laboratorio': laboratorio,
+        'medicamentos': medicamentos,
+        'farmacias': farmacias,
+    })
